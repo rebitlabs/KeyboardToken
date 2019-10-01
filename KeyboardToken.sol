7e489f6a54d339818730c2f28c33b6d89682db00
rebitlabs <55863787+rebitlabs@users.noreply.github.com>
2019-10-01 14:32:45 +0900

    Add files via upload

--- KeyboardToken.sol
+++ KeyboardToken.sol
@@ -14,7 +14,11 @@ contract KeyboardToken is ERC20, ERC20Detailed {
     /**
      * @dev Constructor that gives _msgSender() all of existing tokens.
      */
+    
+    uint8 public constant DECIMALS = 18;
+    uint256 public constant INITIAL_SUPPLY = 7000000000 * (10 ** uint256(DECIMALS));
+
     constructor () public ERC20Detailed("Keyboard Token", "KEYT", 18) {
-        _mint(_msgSender(), 7000000000 * (10 ** uint256(decimals())));
+        _mint(msg.sender, 7000000000 * (10 ** uint256(decimals())));
     }
 }
