c628a44e592dcec69f502f51864f00e19cc65e07
rebitlabs <55863787+rebitlabs@users.noreply.github.com>
2019-10-08 15:19:23 +0900

    Add files via upload

--- KeyboardToken.sol
+++ KeyboardToken.sol
@@ -4,17 +4,13 @@ import "./ERC20.sol";
 import "./ERC20Detailed.sol";
 
 /**
- * @title SimpleToken
+ * @title Keyboard Token
  * @dev Very simple ERC20 Token example, where all tokens are pre-assigned to the creator.
  * Note they can later distribute these tokens as they wish using `transfer` and other
  * `ERC20` functions.
  */
 contract KeyboardToken is ERC20, ERC20Detailed {
 
-    /**
-     * @dev Constructor that gives _msgSender() all of existing tokens.
-     */
-    
     uint8 public constant DECIMALS = 18;
     uint256 public constant INITIAL_SUPPLY = 7000000000 * (10 ** uint256(DECIMALS));
 
