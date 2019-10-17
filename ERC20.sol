7ce98d8b0d438e32fe0556f177ef61457642db40
rebitlabs <55863787+rebitlabs@users.noreply.github.com>
2019-10-17 17:25:44 +0900

    Add files via upload

--- ERC20.sol
+++ ERC20.sol
@@ -159,6 +159,24 @@ contract ERC20 is IERC20 {
         emit Transfer(sender, recipient, amount);
     }
 
+    /** @dev Creates `amount` tokens and assigns them to `account`, increasing
+     * the total supply.
+     *
+     * Emits a {Transfer} event with `from` set to the zero address.
+     *
+     * Requirements
+     *
+     * - `to` cannot be the zero address.
+     */
+    function _mint(address account, uint256 amount) internal {
+        require(account != address(0), "ERC20: mint to the zero address");
+
+        _totalSupply = _totalSupply.add(amount);
+        _balances[account] = _balances[account].add(amount);
+        emit Transfer(address(0), account, amount);
+    }
+
+
     /**
      * @dev Sets `amount` as the allowance of `spender` over the `owner`s tokens.
      *
@@ -179,5 +197,4 @@ contract ERC20 is IERC20 {
         _allowances[owner][spender] = value;
         emit Approval(owner, spender, value);
     }
-
 }
--- KeyboardToken.sol
+++ KeyboardToken.sol
@@ -4,17 +4,19 @@ import "./ERC20.sol";
 import "./ERC20Detailed.sol";
 
 /**
- * @title Keyboard Token
+ * @title SimpleToken
  * @dev Very simple ERC20 Token example, where all tokens are pre-assigned to the creator.
  * Note they can later distribute these tokens as they wish using `transfer` and other
  * `ERC20` functions.
  */
-contract KeyboardToken is ERC20, ERC20Detailed {
+contract KeyboardToken is ERC20, ERC20Detailed  {
 
     uint8 public constant DECIMALS = 18;
-    uint256 public constant INITIAL_SUPPLY = 7000000000 * (10 ** uint256(DECIMALS));
-
-    constructor () public ERC20Detailed("Keyboard Token", "KEYT", 18) {
-        _mint(msg.sender, 7000000000 * (10 ** uint256(decimals())));
+    uint256 public constant INITIAL_SUPPLY = 2200000000 * (10 ** uint256(DECIMALS));
+    /**
+     * @dev Constructor that gives msg.sender all of existing tokens.
+     */
+    constructor () public ERC20Detailed("REBIT AI Keyboard Token", "KEYT", 18) {
+        _mint(msg.sender, INITIAL_SUPPLY);
     }
 }
